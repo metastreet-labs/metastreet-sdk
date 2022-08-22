fce7a64c4fd9915541ddf364481e5da1c4105e37
Andras Szucs <andrew0szucs@gmail.com>
2022-08-22 17:19:58 +0200

    Remove metastreet-sdk apps and generate test

--- apps/test/jest.config.ts
+++ apps/test/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'metastreet-sdk',
+  displayName: 'test',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/metastreet-sdk',
+  coverageDirectory: '../../coverage/apps/test',
 };
