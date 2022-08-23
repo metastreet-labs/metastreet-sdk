59d03b0667f6b6190a3e800e1368f1a7d0f56f1b
Andras Szucs <andrew0szucs@gmail.com>
2022-08-23 15:46:56 +0200

    Add subgraphURI to deployment config in margin-core

--- libs/margin-core/src/lib/deployments.ts
+++ libs/margin-core/src/lib/deployments.ts
@@ -7,19 +7,23 @@ export const CHAIN_IDS = {
 export interface Deployment {
   vaultAddress: string;
   lbWrapperAddress: string;
+  subgraphURI: string;
 }
 
 export const DEPLOYMENTS: Record<number, Deployment> = {
   [CHAIN_IDS.mainnet]: {
     vaultAddress: "",
     lbWrapperAddress: "",
+    subgraphURI: "",
   },
   [CHAIN_IDS.rinkeby]: {
     vaultAddress: "",
     lbWrapperAddress: "",
+    subgraphURI: "",
   },
   [CHAIN_IDS.hardhat]: {
     vaultAddress: "",
     lbWrapperAddress: "",
+    subgraphURI: "",
   },
 };
