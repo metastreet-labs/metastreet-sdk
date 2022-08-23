719546a3d225cc5ef42ba4eb842a6355ebcca74e
Andras Szucs <andrew0szucs@gmail.com>
2022-08-23 17:32:12 +0200

    Add deployment addresses

--- libs/margin-core/src/lib/deployments.ts
+++ libs/margin-core/src/lib/deployments.ts
@@ -12,14 +12,14 @@ export interface Deployment {
 
 export const DEPLOYMENTS: Record<number, Deployment> = {
   [CHAIN_IDS.mainnet]: {
-    vaultAddress: "",
-    lbWrapperAddress: "",
+    vaultAddress: "0x7770cD73e035C37BDf8875eEE81577c63202Ab8d",
+    lbWrapperAddress: "0x28a7C8973CF696D0EEb5f1c92a42B31548daaebb",
     subgraphURI: "",
   },
   [CHAIN_IDS.rinkeby]: {
-    vaultAddress: "",
-    lbWrapperAddress: "",
-    subgraphURI: "",
+    vaultAddress: "0x786E7B3BeD67f32627FE2Cb4aa07c1FB8994581D",
+    lbWrapperAddress: "0xa4F3e9a7ee8100CA430CebAee04B1a998b7cFBe2",
+    subgraphURI: "https://api.studio.thegraph.com/query/31830/metastreet-lb/5.8",
   },
   [CHAIN_IDS.hardhat]: {
     vaultAddress: "",
