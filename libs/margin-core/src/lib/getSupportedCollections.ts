0f1731fe9f4e1bd63ce5e0aa783cea855aa7551a
Abdou Ouahib <ouahibdou@gmail.com>
2022-08-23 19:26:20 +0100

    convert getSupportedCollections to named function

--- libs/margin-core/src/lib/getSupportedCollections.ts
+++ libs/margin-core/src/lib/getSupportedCollections.ts
@@ -1,13 +1,11 @@
 import { LoanPriceOracle__factory, Vault__factory } from "@metastreet-sdk/pe-contracts-typechain";
 import { FetcherParams } from "./types";
 
-const getSupportedCollections = async (params: FetcherParams) => {
+export default async function getSupportedCollections(params: FetcherParams) {
   const { signerOrProvider, deployment } = params;
   const vault = Vault__factory.connect(deployment.vaultAddress, signerOrProvider);
   const loanPriceOracleAddress = await vault.loanPriceOracle();
   const loanPriceOracle = LoanPriceOracle__factory.connect(loanPriceOracleAddress, signerOrProvider);
   const addresses = await loanPriceOracle.supportedCollateralTokens();
   return addresses.map((a) => a.toLowerCase());
-};
-
-export default getSupportedCollections;
+}
