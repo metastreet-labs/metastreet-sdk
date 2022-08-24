c3851c1bea1304a5560119ebda2bb66fc496faab
Abdou Ouahib <ouahibdou@gmail.com>
2022-08-24 12:42:41 +0100

    add refinanceETH transaction

---
+++ libs/margin-core/src/lib/transactions/refinanceETH.ts
@@ -0,0 +1,26 @@
+import { LeverageBuyWrapperV1__factory } from "@metastreet-sdk/pe-contracts-typechain";
+import { BigNumber } from "ethers";
+import { TransactionParams } from "./types";
+
+interface RefinanceETHParams extends TransactionParams {
+  escrowID: string;
+  duration: number;
+  downPayment: string;
+  maxRepayment: string;
+}
+
+const refinanceETH = (params: RefinanceETHParams) => {
+  const { signer, deployment } = params;
+  const contract = LeverageBuyWrapperV1__factory.connect(deployment.lbWrapperAddress, signer);
+  const payable = BigNumber.from(params.downPayment).gt(0);
+  return contract.refinanceETH(
+    params.escrowID,
+    deployment.vaultAddress,
+    params.duration,
+    params.downPayment,
+    params.maxRepayment,
+    { value: payable ? params.downPayment : undefined }
+  );
+};
+
+export default refinanceETH;
