cfdfd28c2c76dd54a1134b9d4809551f64004dc8
Andras Szucs <andrew0szucs@gmail.com>
2022-08-24 18:40:27 +0200

    Lowercase all deployment addresses

--- libs/margin-core/src/lib/deployments.ts
+++ libs/margin-core/src/lib/deployments.ts
@@ -13,13 +13,13 @@ export interface Deployment {
 
 export const DEPLOYMENTS: Record<number, Deployment> = {
   [CHAIN_IDS.mainnet]: {
-    vaultAddress: "0x7770cD73e035C37BDf8875eEE81577c63202Ab8d",
+    vaultAddress: "0x7770cd73e035c37bdf8875eee81577c63202ab8d",
     lbWrapperAddress: "0x28a7C8973CF696D0EEb5f1c92a42B31548daaebb",
     subgraphURI: "",
     reservoirURL: "https://api.reservoir.tools",
   },
   [CHAIN_IDS.rinkeby]: {
-    vaultAddress: "0x786E7B3BeD67f32627FE2Cb4aa07c1FB8994581D",
+    vaultAddress: "0x786e7b3bed67f32627fe2cb4aa07c1fb8994581d",
     lbWrapperAddress: "0xa4F3e9a7ee8100CA430CebAee04B1a998b7cFBe2",
     subgraphURI: "https://api.studio.thegraph.com/query/31830/metastreet-lb/5.8",
     reservoirURL: "https://api-rinkeby.reservoir.tools",
