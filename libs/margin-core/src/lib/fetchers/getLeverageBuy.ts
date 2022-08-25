1e5d7d475407a9153ea51b08d42e7a27bbab6d9b
Abdou Ouahib <56047563+aouahib@users.noreply.github.com>
2022-08-25 12:00:38 +0100

    feat: loan dash fetchers (#8) (#8)
    * add quoteRefinance fetcher
    
    * add refinanceETH transaction
    
    * add isLBWrapperApproved fetcher
    
    * add repayETH transaction
    
    * add getLeverageBuy fetcher

---
+++ libs/margin-core/src/lib/fetchers/getLeverageBuy.ts
@@ -0,0 +1,48 @@
+import { LeverageBuyWrapperV1__factory } from "@metastreet-sdk/pe-contracts-typechain";
+import { FetcherParams } from "./types";
+
+const getPayload = (id: string) => {
+  const payload = {
+    operationName: "LeverageBuy",
+    query: `
+      query LeverageBuy($id: ID!) {
+        leverageBuy(id: $id) {
+          id
+          escrowId
+          status
+          token
+          tokenId
+          tokenURI
+          purchasePrice
+          downpayment
+          principal
+          repayment
+          duration
+          maturity 
+        }
+      }
+    `,
+    variables: { id },
+  };
+  return JSON.stringify(payload);
+};
+
+interface GetLeverageBuyParams extends FetcherParams {
+  escrowID: string;
+}
+
+export const getLeverageBuy = async (params: GetLeverageBuyParams) => {
+  const { signerOrProvider, deployment, escrowID } = params;
+
+  const lbWrapper = LeverageBuyWrapperV1__factory.connect(deployment.lbWrapperAddress, signerOrProvider);
+  const pePlatformAddress = await lbWrapper.purchaseEscrow();
+  const id = `${deployment.lbWrapperAddress}-${pePlatformAddress}-${escrowID}`;
+
+  const response = await fetch(params.deployment.subgraphURI, {
+    method: "POST",
+    body: getPayload(id),
+    headers: { "Content-Type": "application/json" },
+  });
+  // TODO: transform response into a LeverageBuyEntity
+  console.log(response);
+};
---
+++ libs/margin-core/src/lib/transactions/repayETH.ts
@@ -0,0 +1,23 @@
+import {
+  LeverageBuyWrapperV1__factory,
+  PurchaseEscrowPlatformV1__factory,
+} from "@metastreet-sdk/pe-contracts-typechain";
+import { BigNumberish } from "ethers";
+import { TransactionParams } from "./types";
+
+interface RepayETHParams extends TransactionParams {
+  escrowID: string;
+  repayment: BigNumberish;
+}
+
+const repayETH = async (params: RepayETHParams) => {
+  const { deployment, signer } = params;
+
+  const lbWrapper = LeverageBuyWrapperV1__factory.connect(deployment.lbWrapperAddress, signer);
+  const pePlatformAddress = await lbWrapper.purchaseEscrow();
+
+  const pePlatform = PurchaseEscrowPlatformV1__factory.connect(pePlatformAddress, signer);
+  return pePlatform.repayETH(params.escrowID, { value: params.repayment });
+};
+
+export default repayETH;
