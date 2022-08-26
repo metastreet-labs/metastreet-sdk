3c6a49460034423567fa9469536b30883011e888
Andras Szucs <andrew0szucs@gmail.com>
2022-08-26 17:03:45 +0200

    Add useLeverageBuyEvents

--- libs/margin-wagmi/src/index.ts
+++ libs/margin-wagmi/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./lib/DeploymentContext/DeploymentProvider";
 export * from "./lib/DeploymentContext/useDeployment";
 export * from "./lib/queries/useCollateralLimits";
+export * from "./lib/queries/useLeverageBuyEvents";
