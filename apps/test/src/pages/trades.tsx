457efa5839846f57095ab95281fa14faeea44b34
Andras Szucs <andrew0szucs@gmail.com>
2022-08-29 12:11:35 +0200

    Add trades page to test app

---
+++ apps/test/src/pages/trades.tsx
@@ -0,0 +1,87 @@
+import {
+  DeploymentProvider,
+  useCollateralLimits,
+  UseCollateralLimitsParams,
+  useFlashFee,
+  UseFlashFeeParams,
+  useLeverageBuyEvents,
+} from "@metastreet-labs/margin-wagmi";
+import { useProvider } from "wagmi";
+
+const MaxDebt = ({
+  collectionAddress,
+  tokenID,
+  repayment,
+}: Pick<UseCollateralLimitsParams, "collectionAddress" | "tokenID"> & {
+  repayment: UseFlashFeeParams["loanAmount"];
+}) => {
+  const {
+    data: collateralLimits,
+    isLoading: isCollateralLimitsQueryLoading,
+    error: collateralLimitsQueryError,
+  } = useCollateralLimits({ collectionAddress, tokenID });
+  const {
+    data: flashFee,
+    isLoading: isFlashFeeQueryLoading,
+    error: flashFeeQueryError,
+  } = useFlashFee({ loanAmount: repayment });
+
+  if (collateralLimitsQueryError || flashFeeQueryError) {
+    const error = collateralLimitsQueryError ?? flashFeeQueryError;
+    return <p>{error.message}</p>;
+  }
+
+  if (isCollateralLimitsQueryLoading || isFlashFeeQueryLoading) {
+    return <p>Loading...</p>;
+  }
+
+  const maxDebt = collateralLimits.maxPrincipal.sub(flashFee);
+
+  return <p>{`Max Debt: ${maxDebt}`}</p>;
+};
+
+const LeverageBuyEvents = () => {
+  // const { address } = useAccount();
+  const { data } = useLeverageBuyEvents({
+    first: 1000,
+    // owner: address,
+    owner: "0xf2391397fa352cad37023731d37b8013c87592c1",
+    skip: 0,
+  });
+
+  if (!data) {
+    return <p>Loading...</p>;
+  }
+
+  if (data.length <= 0) {
+    return <p>No positions</p>;
+  }
+
+  return (
+    <>
+      {data.map(({ id, leverageBuy: { collectionAddress, tokenID, duration, repayment } }) => (
+        <div key={id}>
+          <p>{`${repayment} wei due in ${duration / 60 / 60 / 24} days`}</p>
+          <MaxDebt collectionAddress={collectionAddress} tokenID={tokenID} repayment={repayment} />
+        </div>
+      ))}
+    </>
+  );
+};
+
+const Trades = () => {
+  const provider = useProvider();
+  /*
+   * Replace the elements below with your own.
+   *
+   * Note: The corresponding styles are in the ./index.css file.
+   */
+  return (
+    <DeploymentProvider chainId={provider.network.chainId}>
+      {provider.network.chainId !== 4 ? <p>Switch to Rinkeby!</p> : null}
+      <LeverageBuyEvents />
+    </DeploymentProvider>
+  );
+};
+
+export default Trades;
