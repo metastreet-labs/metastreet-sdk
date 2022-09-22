2fb395f770a8c433318f902f511567780c383706
Abdou Ouahib <ouahibdou@gmail.com>
2022-09-22 16:24:42 +0100

    fix ts error

--- libs/margin-kit/src/lib/lib/hooks/useDeployment.ts
+++ libs/margin-kit/src/lib/lib/hooks/useDeployment.ts
@@ -1,10 +1,10 @@
-import { DEPLOYMENTS } from "@metastreet-labs/margin-core";
+import { Deployment, DEPLOYMENTS } from "@metastreet-labs/margin-core";
 import { useProvider } from "wagmi";
 
 const useDeployment = () => {
   const provider = useProvider();
   const chainID = provider.network.chainId;
-  const deployment = DEPLOYMENTS[chainID] ?? {};
+  const deployment = DEPLOYMENTS[chainID] ?? ({} as Deployment);
 
   return { provider, chainID, deployment };
 };
