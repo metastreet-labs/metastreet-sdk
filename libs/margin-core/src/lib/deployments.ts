95aa080770a0b5d3bed75022fc45b85cf2b57e36
Abdou Ouahib <ouahibdou@gmail.com>
2022-10-17 16:42:16 +0100

    update deployments

--- libs/margin-core/src/lib/deployments.ts
+++ libs/margin-core/src/lib/deployments.ts
@@ -15,13 +15,13 @@ export const DEPLOYMENTS: Record<number, Deployment> = {
   [CHAIN_IDS.mainnet]: {
     vaultAddress: "0x7770cd73e035c37bdf8875eee81577c63202ab8d",
     lbWrapperAddress: "0x558aD8278B6D127dc8F58e02Ee578Df20ec98406",
-    subgraphURI: "https://api.studio.thegraph.com/query/26817/metastreet-lb-alpha/v0.0.3",
+    subgraphURI: "https://api.studio.thegraph.com/query/26817/metastreet-powersweep/v1.0.0",
     reservoirURL: "https://api.reservoir.tools",
   },
   [CHAIN_IDS.goerli]: {
     vaultAddress: "0x99156878c19c46c492d3ac40a3a8483c3740a9d5",
-    lbWrapperAddress: "0xEA149987421D9F07897bE4739Ce9431AFf8642c2",
-    subgraphURI: "https://api.studio.thegraph.com/query/31830/metastreet-lb-goerli/0.11",
+    lbWrapperAddress: "0x5D17Fb028eB1D4aC437e2E94aB0A47146d6cC2Ce",
+    subgraphURI: "https://api.studio.thegraph.com/query/31830/metastreet-lb-goerli/0.12",
     reservoirURL: "https://api-goerli.reservoir.tools",
   },
   [CHAIN_IDS.hardhat]: {
