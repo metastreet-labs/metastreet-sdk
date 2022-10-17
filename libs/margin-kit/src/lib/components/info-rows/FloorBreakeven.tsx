fb84454c2526d846c30fe4de5831528df6082dc3
Abdou Ouahib <ouahibdou@gmail.com>
2022-10-17 10:23:06 +0100

    include fees in floor breakeven

--- libs/margin-kit/src/lib/components/info-rows/FloorBreakeven.tsx
+++ libs/margin-kit/src/lib/components/info-rows/FloorBreakeven.tsx
@@ -1,17 +1,27 @@
 import { ReactNode } from "react";
+import useDefinedMetaStreetDeployment from "../../hooks/useDefinedMetaStreetDeployment";
+import useFeesQuery from "../../lib/hooks/useFeesQuery";
 import { prettyFormatNumber } from "../../utils/numbers";
 import useBuyWithLeverage from "../BuyWithLeverageModal/state/useBuyWithLeverage";
 import ETHPrice from "../ETHPrice";
+import QuestionMarkCircleIcon from "../icons/QuestionMarkCircleIcon";
 import { InfoRow, InfoRowLabel, InfoRowValue } from "../InfoRow";
 import LoadingText from "../LoadingText";
+import Tooltip from "../Tooltip";
 
 const FloorBreakeven = () => {
-  const { formState } = useBuyWithLeverage();
+  const { chainID } = useDefinedMetaStreetDeployment();
+  const { formState, tokens } = useBuyWithLeverage();
   const { totalDownPayment, totalRepayment } = formState;
+  const { collectionAddress } = tokens[0];
+  const { data: fees } = useFeesQuery({ collectionAddress, chainID });
 
   let floorBreakeven: ReactNode;
-  if (totalRepayment) {
-    const floorBreakevenAmount = prettyFormatNumber(totalDownPayment + totalRepayment);
+  if (totalRepayment && fees) {
+    const floorBreakevenWithoutFees = (totalDownPayment + totalRepayment) / tokens.length;
+    const openSeaFee = (floorBreakevenWithoutFees * fees.opensea.bps) / 10000;
+    const royalty = (floorBreakevenWithoutFees * fees.royalty.bps) / 10000;
+    const floorBreakevenAmount = prettyFormatNumber(floorBreakevenWithoutFees + openSeaFee + royalty);
     floorBreakeven = <ETHPrice price={floorBreakevenAmount} />;
   } else {
     floorBreakeven = <LoadingText />;
@@ -19,7 +29,14 @@ const FloorBreakeven = () => {
 
   return (
     <InfoRow className="bwl-modal-form-floor-breakeven">
-      <InfoRowLabel>Floor Breakeven</InfoRowLabel>
+      <InfoRowLabel>
+        {tokens.length > 1 ? "Floor Breakeven (Per Asset)" : "Floor Breakeven"}
+        <Tooltip
+          className="cursor-help ml-1"
+          trigger={<QuestionMarkCircleIcon className="w-4 h-4" />}
+          tooltipText="Includes OpenSea fees and royalties"
+        />
+      </InfoRowLabel>
       <InfoRowValue>{floorBreakeven}</InfoRowValue>
     </InfoRow>
   );
---
+++ libs/margin-kit/src/lib/lib/hooks/useFeesQuery.ts
@@ -0,0 +1,24 @@
+import { useQuery } from "wagmi";
+import getFees, { GetFeesParams, GetFeesResult } from "../fetchers/getFees";
+
+export type UseFeesQueryParams = GetFeesParams;
+
+export default function useFeesQuery(params: UseFeesQueryParams) {
+  return useQuery<GetFeesResult, Error>(
+    useFeesQueryKeys.collection(params),
+    () => getFees(params),
+    // no need to refetch at all
+    {
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: 86400,
+    }
+  );
+}
+
+export const useFeesQueryKeys = {
+  all: (chainID: number) => ["openSea_fees", chainID],
+  collection: (params: GetFeesParams) => [...useFeesQueryKeys.all(params.chainID), params.collectionAddress],
+};
