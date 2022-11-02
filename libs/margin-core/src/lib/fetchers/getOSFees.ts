cd27e120ad0933a33dc7399a657c5dd2e2cd760b
Abdou Ouahib <ouahibdou@gmail.com>
2022-11-02 14:40:17 +0100

    add getOSFees

---
+++ libs/margin-core/src/lib/fetchers/getOSFees.ts
@@ -0,0 +1,48 @@
+import { ethers } from "ethers";
+import { withReadableError } from "../errors";
+
+export interface GetFeesParams {
+  collectionAddress: string;
+  chainID: number;
+}
+
+export interface Fees {
+  recipient: string;
+  bps: number;
+}
+
+export interface GetFeesResult {
+  royalty: Fees;
+  opensea: Fees;
+}
+
+const ZERO_FEES: Fees = {
+  recipient: ethers.constants.AddressZero,
+  bps: 0,
+};
+
+const _getOSFees = async (params: GetFeesParams): Promise<GetFeesResult> => {
+  const OS_V1_API_URL_MAINNET = "https://api.opensea.io/api/v1";
+  const OS_V1_API_URL_GOERLI = "https://testnets-api.opensea.io/api/v1";
+
+  const url = params.chainID == 1 ? OS_V1_API_URL_MAINNET : OS_V1_API_URL_GOERLI;
+  const response = await fetch(`${url}/asset_contract/${params.collectionAddress}`);
+  const json = await response.json();
+  if (!response.ok) throw json;
+
+  const { seller_fees, opensea_fees } = json.collection.fees;
+
+  let royalty: Fees;
+  const royaltyRecipient = Object.keys(seller_fees)[0];
+  if (royaltyRecipient) royalty = { recipient: royaltyRecipient, bps: seller_fees[royaltyRecipient] };
+  else royalty = ZERO_FEES;
+
+  let opensea: Fees;
+  const openseaFeeRecipient = Object.keys(opensea_fees)[0];
+  if (openseaFeeRecipient) opensea = { recipient: openseaFeeRecipient, bps: opensea_fees[openseaFeeRecipient] };
+  else opensea = ZERO_FEES;
+
+  return { royalty, opensea };
+};
+
+export const getOSFees = withReadableError(_getOSFees);
