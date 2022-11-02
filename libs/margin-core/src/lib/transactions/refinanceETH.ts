15fb31e54ff9163f9ef8bb1f657661e999abb70c
Abdou Ouahib <ouahibdou@gmail.com>
2022-11-02 12:23:28 +0100

    add refinanceETH

--- libs/margin-core/src/lib/transactions/refinanceETH.ts
+++ libs/margin-core/src/lib/transactions/refinanceETH.ts
@@ -1,27 +1,35 @@
-import { BigNumberish, ContractTransaction } from "ethers";
+import {
+  LeverageBuyWrapperV1__factory,
+  PurchaseEscrowPlatform__factory,
+} from "@metastreet-labs/pe-contracts-typechain";
+import { BigNumber, BigNumberish, ethers } from "ethers";
 import { withReadableError } from "../errors";
 import { TransactionParams } from "./types";
 
-export interface RefinanceETHParams extends TransactionParams {
-  escrowID: string;
-  duration: number;
+type RefinanceETHParams = TransactionParams & {
+  escrowID: BigNumberish;
+  duration: BigNumberish;
   downPayment: BigNumberish;
   maxRepayment: BigNumberish;
-}
+};
 
-const _refinanceETH = (params: RefinanceETHParams): Promise<ContractTransaction> => {
-  throw new Error("Not implemented");
-  //const { signer, deployment } = params;
-  //const contract = LeverageBuyWrapperV1__factory.connect(deployment.lbWrapperAddress, signer);
-  //const payable = BigNumber.from(params.downPayment).gt(0);
-  //return contract.refinanceETH(
-  //  params.escrowID,
-  //  deployment.vaultAddress,
-  //  params.duration,
-  //  params.downPayment,
-  //  params.maxRepayment,
-  //  { value: payable ? params.downPayment : undefined }
-  //);
+const _refinanceETH = async (params: RefinanceETHParams) => {
+  const { deployment, signer } = params;
+
+  const lbWrapper = LeverageBuyWrapperV1__factory.connect(deployment.lbWrapperAddress, signer);
+  const pePlatformAddress = await lbWrapper.purchaseEscrow();
+  const pePlatform = PurchaseEscrowPlatform__factory.connect(pePlatformAddress, signer);
+
+  const calldata = ethers.utils.defaultAbiCoder.encode(
+    ["address", "uint64", "int256", "uint256"],
+    [deployment.vaultAddress, params.duration, params.downPayment, params.maxRepayment]
+  );
+
+  const payable = BigNumber.from(params.downPayment).gt(0);
+
+  return pePlatform.transferAndCall(params.escrowID, deployment.lbWrapperAddress, calldata, {
+    value: payable ? params.downPayment : undefined,
+  });
 };
 
 export const refinanceETH = withReadableError(_refinanceETH);
