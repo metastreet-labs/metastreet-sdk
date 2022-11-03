c4d24243aec95b45cf871a1a585fb9e8b4491b87
Abdou Ouahib <ouahibdou@gmail.com>
2022-11-03 14:17:46 +0100

    rename util function

--- libs/margin-kit/src/lib/utils/dates.ts
+++ libs/margin-kit/src/lib/utils/dates.ts
@@ -2,4 +2,4 @@ const SECONDS_IN_ONE_DAY = 86400;
 
 export const daysFromSeconds = (seconds: number) => Math.ceil(seconds / SECONDS_IN_ONE_DAY);
 
-export const secondsFromDays = (days: number) => days * SECONDS_IN_ONE_DAY;
+export const daysToSeconds = (days: number) => days * SECONDS_IN_ONE_DAY;
