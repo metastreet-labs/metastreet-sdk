dd281897c36ee47425e71aa3f59a33823e537896
Abdou Ouahib <ouahibdou@gmail.com>
2022-11-04 16:45:22 +0100

    add RefinanceTokenInfo component

--- libs/margin-kit/src/lib/components/RefinanceModal/RefinanceModalContent/RefinanceModalContent.tsx
+++ libs/margin-kit/src/lib/components/RefinanceModal/RefinanceModalContent/RefinanceModalContent.tsx
@@ -1,13 +1,18 @@
 import MetaStreetModal from "../../MetaStreetModal";
+import RefinanceTokenInfo from "../../token-info/RefinanceTokenInfo";
+import useRefinance from "../state/useRefinance";
 
 interface RefinanceModalContentProps {
   onClose: () => void;
 }
 
 const RefinanceModalContent = (props: RefinanceModalContentProps) => {
+  const { leverageBuy } = useRefinance();
+
   return (
     <>
       <MetaStreetModal.Title>Refinance</MetaStreetModal.Title>
+      <RefinanceTokenInfo leverageBuy={leverageBuy} />
       <div className="flex h-56 items-center justify-center">This is a refinance modal</div>
     </>
   );
