41ca9ad16574db2b8d3130928c0c92600535bf42
Abdou Ouahib <56047563+aouahib@users.noreply.github.com>
2022-11-07 13:20:21 +0100

    feat: useRefinanceForm (#76) (#78)
    * add useQuoteRefinance and its debounced version
    
    * add useRefinanceForm
    
    * rename util function

---
+++ libs/margin-kit/src/lib/utils/dates.ts
@@ -0,0 +1,5 @@
+const SECONDS_IN_ONE_DAY = 86400;
+
+export const daysFromSeconds = (seconds: number) => Math.ceil(seconds / SECONDS_IN_ONE_DAY);
+
+export const daysToSeconds = (days: number) => days * SECONDS_IN_ONE_DAY;
