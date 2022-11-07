16e6da440680b06d429a67774ad4677738587bea
Abdou Ouahib <ouahibdou@gmail.com>
2022-11-07 16:30:07 +0100

    simplify test page

--- apps/test/src/pages/leverage-buys.tsx
+++ apps/test/src/pages/leverage-buys.tsx
@@ -1,7 +1,5 @@
-import { ArrowPathIcon } from "@heroicons/react/24/solid";
 import { LeverageBuy } from "@metastreet-labs/margin-core";
 import { useLeverageBuys } from "@metastreet-labs/margin-kit";
-import classNames from "classnames";
 import { NextPage } from "next";
 import { useState } from "react";
 import { useQuery } from "wagmi";
@@ -9,11 +7,17 @@ import { useQuery } from "wagmi";
 const LeverageBuysPage: NextPage = () => {
   const { data } = useLeverageBuys();
   return (
-    <div className="w-screen h-screen overflow-hidden bg-black">
+    <table className="border-spacing-y-2 border-spacing-x-4 border border-separate">
+      <tr>
+        <th>Image</th>
+        <th>Collection Name</th>
+        <th>Token ID</th>
+        <th></th>
+      </tr>
       {data?.map((lb) => (
         <LBRow leverageBuy={lb} key={lb.escrowID} />
       ))}
-    </div>
+    </table>
   );
 };
 
@@ -27,20 +31,18 @@ const LBRow = (props: LBRowProps) => {
   const [refiModalOpen, setRefiModalOpen] = useState(false);
 
   return (
-    <div className="flex items-center text-white space-x-4">
-      <TokenImage src={data?.image} className="w-14 h-14 rounded-md" />
-      <div className="flex flex-col justify-center font-semibold">
-        <span className="text-sm">{data?.name ? data?.name : "..."}</span>
-        <span className="text-xs">#{leverageBuy.tokenID}</span>
-      </div>
-      <button
-        className="flex items-center space-x-1 border border-orange-400 rounded px-2 py-1 text-orange-400"
-        onClick={() => setRefiModalOpen(true)}
-      >
-        <ArrowPathIcon className="w-4 h-4" />
-        <span>Refinance</span>
-      </button>
-    </div>
+    <tr>
+      <td>
+        <img src={data?.image} alt="" className="w-10 h-10" />
+      </td>
+      <td>{data?.name ?? "..."}</td>
+      <td>{leverageBuy.tokenID}</td>
+      <td>
+        <button className="border" onClick={() => setRefiModalOpen(true)}>
+          Refinance
+        </button>
+      </td>
+    </tr>
   );
 };
 
@@ -65,33 +67,6 @@ const useTokenMetadata = (tokenURI: string) => {
     return json;
   };
   return useQuery<TokenMetadata, unknown>(["token-metadata", url], fetcher);
-};
-
-interface TokenImageProps {
-  src?: string;
-  className?: string;
-}
-
-const TokenImage = (props: TokenImageProps) => {
-  const { src, className } = props;
-  const [loaded, setLoaded] = useState(false);
-
-  return (
-    <div className={classNames("flex relative overflow-hidden", className)}>
-      <img src="/token-image-placeholder.png" alt="" className="object-cover object-center w-full h-full" />
-      {src ? (
-        <img
-          src={src}
-          alt=""
-          onLoad={() => setLoaded(true)}
-          className={classNames("transition-opacity duration-1000 absolute z-10 object-cover w-full h-full", {
-            "opacity-0": !loaded,
-            "opacity-100": loaded,
-          })}
-        />
-      ) : null}
-    </div>
-  );
 };
 
 export default LeverageBuysPage;
