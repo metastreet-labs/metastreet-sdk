ba33a6a38763685a3952fccd57a3edfd9da59921
Abdou Ouahib <ouahibdou@gmail.com>
2022-11-09 13:15:24 +0100

    add useSupportingVaults

--- libs/margin-kit/src/index.ts
+++ libs/margin-kit/src/index.ts
@@ -18,6 +18,7 @@ export { default as useIsCollectionSupported } from "./lib/lib/hooks/useIsCollec
 export * from "./lib/lib/hooks/useLeverageBuys";
 export { default as useOSFlagged } from "./lib/lib/hooks/useOSFlagged";
 export { default as useSupportedCollections } from "./lib/lib/hooks/useSupportedCollections";
+export * from "./lib/lib/hooks/useSupportingVaults";
 export * from "./lib/lib/hooks/useTokenMetadata";
 export * from "./lib/lib/hooks/useVaultsLimits";
 export * from "./lib/lib/hooks/useVaultsSupportedCollections";
