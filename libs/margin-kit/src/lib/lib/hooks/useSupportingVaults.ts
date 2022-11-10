3d673e37d293bfb7eba31e52c37c049a23982c99
Abdou Ouahib <ouahibdou@gmail.com>
2022-11-10 12:41:32 +0100

    move unsupported collection error to useSupportingVaults hook

--- libs/margin-kit/src/lib/lib/hooks/useSupportingVaults.ts
+++ libs/margin-kit/src/lib/lib/hooks/useSupportingVaults.ts
@@ -1,13 +1,22 @@
 import { useVaultsSupportedCollections } from "./useVaultsSupportedCollections";
 
 export const useSupportingVaults = (collectionAddress: string) => {
-  const { data: vaultsSupportedCollections, ...rest } = useVaultsSupportedCollections();
+  const {
+    data: vaultsSupportedCollections,
+    error: vaultsSupportedCollectionsError,
+    ...rest
+  } = useVaultsSupportedCollections();
 
   let data: string[] | undefined;
+  let error = vaultsSupportedCollectionsError;
   if (vaultsSupportedCollections) {
     const { all, ...withoutAll } = vaultsSupportedCollections;
     data = Object.keys(withoutAll).filter((vaultAddress) => withoutAll[vaultAddress].includes(collectionAddress));
+    if (data.length == 0) {
+      data = undefined;
+      error = { message: "Unsupported collections", originalError: new Error("Unsupported collections") };
+    }
   }
 
-  return { data, ...rest };
+  return { data, error, ...rest };
 };
--- libs/margin-kit/src/lib/lib/hooks/useSupportingVaultsLimits.ts
+++ libs/margin-kit/src/lib/lib/hooks/useSupportingVaultsLimits.ts
@@ -1,4 +1,3 @@
-import { ReadableError } from "@metastreet-labs/margin-core";
 import { useSupportingVaults } from "./useSupportingVaults";
 import { useVaultsLimits, VaultLimits } from "./useVaultsLimits";
 
@@ -11,19 +10,10 @@ export const useSupportingVaultsLimits = (params: UseSupportingVaultsLimitsParam
   const { data: supportingVaults, error: supportingVaultsError } = useSupportingVaults(params.collectionAddress);
   const { data: allVaultsLimits, error: allVaultsLimitsError } = useVaultsLimits(params);
 
-  let error: ReadableError | null = supportingVaultsError ?? allVaultsLimitsError;
   let data: VaultLimits[] | undefined;
   if (supportingVaults && allVaultsLimits) {
-    // get limits of vaults that support collectionAddress
     data = allVaultsLimits.filter((limit) => supportingVaults.includes(limit.vaultAddress));
-    // if no vault supports collectionAddress
-    if (data.length == 0) {
-      // set data to undefined
-      data = undefined;
-      // and error to "Unsupported collection"
-      error = { message: "Unsupported collection", originalError: new Error("Unsupported collection") };
-    }
   }
 
-  return { data, error };
+  return { data, error: supportingVaultsError ?? allVaultsLimitsError };
 };
