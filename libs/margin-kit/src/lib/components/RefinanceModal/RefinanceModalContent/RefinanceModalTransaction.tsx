bc69180be42e524c4453c945472c299c69bd3c71
Abdou Ouahib <ouahibdou@gmail.com>
2022-11-11 15:49:55 +0100

    add refinance modal transaction

---
+++ libs/margin-kit/src/lib/components/RefinanceModal/RefinanceModalContent/RefinanceModalTransaction.tsx
@@ -0,0 +1,14 @@
+import TransactionSteps from "../../TransactionSteps";
+import useRefinance from "../state/useRefinance";
+
+interface RefinanceModalTransactionProps {
+  onClose: () => void;
+}
+
+const RefinanceModalTransaction = (props: RefinanceModalTransactionProps) => {
+  const { transactionState } = useRefinance();
+
+  return <TransactionSteps state={transactionState} onClose={props.onClose} />;
+};
+
+export default RefinanceModalTransaction;
