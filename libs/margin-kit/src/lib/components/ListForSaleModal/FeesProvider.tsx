d729bfe855edee393a694f2093372764e67c516e
Abdou Ouahib <ouahibdou@gmail.com>
2022-11-15 12:09:43 +0100

    add FeesProvider component

---
+++ libs/margin-kit/src/lib/components/ListForSaleModal/FeesProvider.tsx
@@ -0,0 +1,29 @@
+import { GetFeesResult } from "@metastreet-labs/margin-core";
+import { createContext, PropsWithChildren, useContext } from "react";
+import useDefinedMetaStreetDeployment from "../../hooks/useDefinedMetaStreetDeployment";
+import useFeesQuery from "../../lib/hooks/useFeesQuery";
+import ModalLoadingOrError from "../ModalLoadingOrError";
+
+const FeesContext = createContext<GetFeesResult | undefined>(undefined);
+
+export const useFees = () => {
+  const context = useContext(FeesContext);
+  if (!context) throw new Error("useFees was used outside of a FeesProvider");
+  return context;
+};
+
+type FeesProviderProps = PropsWithChildren & {
+  collectionAddress: string;
+};
+
+const FeesProvider = (props: FeesProviderProps) => {
+  const { children, collectionAddress } = props;
+  const { chainID } = useDefinedMetaStreetDeployment();
+  const { data, error } = useFeesQuery({ collectionAddress, chainID });
+
+  if (!data) return <ModalLoadingOrError error={error?.message} />;
+
+  return <FeesContext.Provider value={data}>{children}</FeesContext.Provider>;
+};
+
+export default FeesProvider;
