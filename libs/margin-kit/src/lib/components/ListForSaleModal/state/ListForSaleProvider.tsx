4f69b8ee80eb08cf886067b71189073f4900ef09
Abdou Ouahib <ouahibdou@gmail.com>
2022-11-15 13:00:22 +0100

    add ListForSaleProvider

---
+++ libs/margin-kit/src/lib/components/ListForSaleModal/state/ListForSaleProvider.tsx
@@ -0,0 +1,32 @@
+import { LeverageBuy } from "@metastreet-labs/margin-core";
+import { PropsWithChildren } from "react";
+import { ListForSaleContext, ListForSaleContextType } from "./ListForSaleContext";
+import { useListForSaleForm } from "./useListForSaleForm";
+import { useListForSaleTransaction, UseListForSaleTransactionParams } from "./useListForSaleTransaction";
+
+type ListForSaleProviderProps = PropsWithChildren & {
+  leverageBuy: LeverageBuy;
+  postOrderToOpenSea: UseListForSaleTransactionParams["postOrderToOpenSea"];
+};
+
+const ListForSaleProvider = (props: ListForSaleProviderProps) => {
+  const { children, leverageBuy, postOrderToOpenSea } = props;
+  const { formState, formActions } = useListForSaleForm({ leverageBuy });
+  const { transactionState, listForSale } = useListForSaleTransaction({
+    formState,
+    formActions,
+    escrowID: leverageBuy.escrowID,
+    postOrderToOpenSea,
+  });
+
+  const contextValue: ListForSaleContextType = {
+    formState,
+    transactionState,
+    actions: { ...formActions, listForSale },
+    leverageBuy,
+  };
+
+  return <ListForSaleContext.Provider value={contextValue}>{children}</ListForSaleContext.Provider>;
+};
+
+export default ListForSaleProvider;
