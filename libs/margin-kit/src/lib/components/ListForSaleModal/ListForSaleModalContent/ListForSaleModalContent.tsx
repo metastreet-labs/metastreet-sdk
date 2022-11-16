e51847dfdab8853303f9bbf1ca7f9f3ea3b92c66
Abdou Ouahib <ouahibdou@gmail.com>
2022-11-16 12:09:04 +0100

    add list for sale modal success

--- libs/margin-kit/src/lib/components/ListForSaleModal/ListForSaleModalContent/ListForSaleModalContent.tsx
+++ libs/margin-kit/src/lib/components/ListForSaleModal/ListForSaleModalContent/ListForSaleModalContent.tsx
@@ -3,6 +3,7 @@ import MetaStreetModal from "../../MetaStreetModal";
 import LeverageBuyTokenInfo from "../../token-info/LeverageBuyTokenInfo";
 import { useListForSale } from "../state/ListForSaleContext";
 import ListForSaleModalForm from "./ListForSaleModalForm";
+import ListForSaleModalSuccess, { ListForSaleModalSuccessAnimation } from "./ListForSaleModalSuccess";
 import ListForSaleModalTransaction from "./ListForSaleModalTransaction";
 
 interface ListForSaleModalContentProps {
@@ -16,11 +17,11 @@ const ListForSaleModalContent = (props: ListForSaleModalContentProps) => {
   return (
     <>
       <MetaStreetModal.Title>List for Sale</MetaStreetModal.Title>
-      {status == "complete" ? "<ListForSaleSuccessAnimation />" : <LeverageBuyTokenInfo leverageBuy={leverageBuy} />}
+      {status == "complete" ? <ListForSaleModalSuccessAnimation /> : <LeverageBuyTokenInfo leverageBuy={leverageBuy} />}
       <Divider className="bwl-modal-content-divider" />
       {status == "idle" ? <ListForSaleModalForm /> : null}
       {status == "loading" || status == "error" ? <ListForSaleModalTransaction onClose={props.onClose} /> : null}
-      {status == "complete" ? "<ListForSaleModalSuccess />" : null}
+      {status == "complete" ? <ListForSaleModalSuccess onClose={props.onClose} /> : null}
     </>
   );
 };
