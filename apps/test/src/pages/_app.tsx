b885a6b5204156123767e725de9e7752b0304b4e
Abdou Ouahib <ouahibdou@gmail.com>
2022-11-20 15:46:43 +0100

    handle hydration error in _app

--- apps/test/src/pages/_app.tsx
+++ apps/test/src/pages/_app.tsx
@@ -3,14 +3,19 @@ import { AppProps } from "next/app";
 import Head from "next/head";
 import Connect from "../components/Connect";
 import WagmiProvider from "../components/WagmiProvider";
+import useMounted from "../hooks/useMounted";
 import "../styles/styles.css";
 
 const CustomApp = ({ Component, pageProps }: AppProps) => {
+  // Sad workaround to fix hydration error
+  const mounted = useMounted();
+  if (!mounted) return null;
+
   return (
     <WagmiProvider>
       <DeploymentProvider>
         <Head>
-          <title>Welcome to test!</title>
+          <title>MetaStreet SDK Demo</title>
         </Head>
         <main className="app">
           <Connect>
