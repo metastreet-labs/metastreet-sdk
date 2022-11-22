0133910b95401115a6e09553859c7557d93c3d97
Abdou Ouahib <ouahibdou@gmail.com>
2022-11-22 22:57:23 +0100

    chore: add comments

--- apps/test/src/components/BuyWithLeverageSection.tsx
+++ apps/test/src/components/BuyWithLeverageSection.tsx
@@ -4,20 +4,32 @@ import Button from "./Button";
 import Input from "./Input";
 
 const BuyWithLeverageSection = () => {
+  // deployment object, grabbed from the DeploymentProvider that wraps the app
   const deployment = useDeployment();
+  // value of the collection address input, defaults to Goerli's MultiFaucet NFT
   const [collectionAddress, setCollectionAddress] = useState("0xf5de760f2e916647fd766b4ad9e85ff943ce3a2b");
+  // value of the token IDs input, defaults to 3 random token IDs
   const [tokenIDs, setTokenIDs] = useState("958576,1390026,940282");
+  // the fetched tokens
   const [tokens, setTokens] = useState<BWLToken[]>([]);
 
+  // called when the fetch button is clicked
+  // it fetches NFT objects based on the entered collection address and token IDs
   const fetchToken = async () => {
+    // if deployment is undefined, either it's an unsupported network,
+    // or there was no DeploymentProvider up the component tree
     if (!deployment) throw new Error("Deployment is undefined (unsupported network)");
+    // construct query params
     const params = new URLSearchParams();
     const tokenIDsArray = tokenIDs.split(",");
     tokenIDsArray.forEach((id) => params.append("tokens", `${collectionAddress}:${id}`));
+    // make the call to reservoir API, we use this endpoint: https://docs.reservoir.tools/reference/gettokensv5
+    // it's not required to fetch tokens from reservoir API, they can be fetched from any other supported marketplace API.
     const url = `${deployment.reservoirURL}/tokens/v5?${params}`;
     const response = await fetch(url);
     if (response.ok) {
       const data = await response.json();
+      // Construct BWLTokens from the fetched tokens
       const tokens = (data.tokens as any[])
         .map((token) => {
           // required fields
@@ -27,6 +39,7 @@ const BuyWithLeverageSection = () => {
           // Optional fields
           const tokenImage = token?.token?.image ?? "/token-image-placeholder.png";
           const collectionName = token?.token?.collection?.name ?? "Unnamed";
+          // add the token to the array if all required fields are present
           if (tokenID != undefined && tokenPrice && collectionAddress) {
             return {
               tokenID,
@@ -36,8 +49,11 @@ const BuyWithLeverageSection = () => {
               collectionName,
             };
           }
+          // otherwise undefined will be added to the array
         })
+        // remove all undefined values from the array
         .filter((token) => token != undefined) as BWLToken[];
+      // save the fetched tokens in state
       setTokens(tokens);
     }
   };
@@ -46,16 +62,20 @@ const BuyWithLeverageSection = () => {
     <section className="flex flex-col space-y-1">
       <h2 className="text-2xl font-bold">Buy With Leverage</h2>
       <div className="flex space-x-2 items-center">
+        {/* The collection address input */}
         <span>Collection address:</span>
         <Input value={collectionAddress} onChange={(e) => setCollectionAddress(e.target.value)} />
+        {/* The tokenIDs input */}
         <span>tokenIDs (comma separated): </span>
         <Input value={tokenIDs} onChange={(e) => setTokenIDs(e.target.value)} />
+        {/* the fetch button */}
         <Button type="button" onClick={fetchToken}>
           Fetch tokens
         </Button>
       </div>
       <div className="flex items-start">
-        Fetched tokens:{" "}
+        Fetched tokens:
+        {/* if there are fetched tokens, display them in a list. If not, display "none" */}
         {tokens.length ? (
           <div className="flex flex-col ml-4 space-y-1">
             {tokens.map((token) => (
@@ -65,6 +85,7 @@ const BuyWithLeverageSection = () => {
         ) : (
           "none"
         )}
+        {/* this wraps a BuyWithLeverageButton and BuyWithLeverageModal components */}
         <BuyWithLeverage tokens={tokens} className="w-56 ml-4" />
       </div>
     </section>
@@ -75,6 +96,7 @@ interface TokenRowProps {
   token: BWLToken;
 }
 
+// TokenRow is just a component representing a single fetched NFT
 const TokenRow = (props: TokenRowProps) => {
   const { token } = props;
 
--- apps/test/src/components/PositionsSection.tsx
+++ apps/test/src/components/PositionsSection.tsx
@@ -6,7 +6,9 @@ import { useNetwork, useQuery, useSigner } from "wagmi";
 import Button from "./Button";
 
 const PositionsSection = () => {
+  // a list of all active LeverageBuys (loans) of the connected address
   const { data } = useLeverageBuys();
+
   return (
     <section className="flex flex-col space-y-2">
       <h1 className="text-2xl font-bold">Your Positions</h1>
@@ -36,15 +38,23 @@ interface LBRowProps {
 
 const LBRow = (props: LBRowProps) => {
   const { leverageBuy } = props;
-  const { data } = useTokenMetadata(leverageBuy.tokenURI);
-  const [refiModalOpen, setRefiModalOpen] = useState(false);
-  const { data: signer } = useSigner();
+  // deployment object, grabbed from the DeploymentProvider that wraps the app
   const deployment = useDeployment();
+  // current connected signer
+  const { data: signer } = useSigner();
+  // token metadata of this leverageBuy's underlying NFT (name, image, ...)
+  const { data } = useTokenMetadata(leverageBuy.tokenURI);
+  // RefinanceModal state
+  const [refiModalOpen, setRefiModalOpen] = useState(false);
 
+  // this leverageBuy's listing data, can be undefined if the underlying NFT is not listed for sale
   const { listingData } = leverageBuy;
+  // calculate the remaining time before listing expiration
   const listingTimeRemaining = listingData && Math.floor((listingData.endTime - new Date().getTime() / 1000) / 86400);
+  // the NFT is listed if listing data is not undefined, and remaining time is greater than 0
   const isListed = listingData && listingTimeRemaining && listingTimeRemaining > 0;
 
+  // called when the repay button is clicked, it initiates a transaction to repay the loan
   const repay = () => {
     if (!signer) throw new Error("repay called without a signer");
     if (!deployment) throw new Error("repay was called without a deployment");
@@ -58,19 +68,24 @@ const LBRow = (props: LBRowProps) => {
 
   return (
     <tr>
+      {/* this leverageBuy's underlying NFT's image */}
       <td>
         <img src={data?.image} alt="" className="w-10 h-10" />
       </td>
+      {/* this leverageBuy's underlying NFT name and ID */}
       <td>
         {data?.name ?? "..."} #{leverageBuy.tokenID}
       </td>
+      {/* Repay button */}
       <td>
         <Button onClick={repay}>Repay</Button>
       </td>
+      {/* Refinance button and modal */}
       <td>
         <Button onClick={() => setRefiModalOpen(true)}>Refinance</Button>
         <RefinanceModal isOpen={refiModalOpen} onClose={() => setRefiModalOpen(false)} leverageBuy={leverageBuy} />
       </td>
+      {/* Render different components depending on whether the NFT is listed or not */}
       {isListed ? (
         <ListedLB leverageBuy={{ ...leverageBuy, listingData }} />
       ) : (
@@ -84,15 +99,20 @@ interface ListedLBProps {
   leverageBuy: LeverageBuy & { listingData: ListingData };
 }
 
+// ListedLB is rendered if the NFT is listed for sale,
+// it displays how much it's listed for, as well as a button to cancel the listing
 const ListedLB = ({ leverageBuy }: ListedLBProps) => {
+  // current chain
   const { chain } = useNetwork();
+  // connected signer
   const { data: signer } = useSigner();
+  // deployment object, grabbed from the DeploymentProvider that wraps the app
   const deployment = useDeployment();
 
+  // called when the Delist button is clicked, it initiates a transaction to cancel the listing
   const delist = () => {
     if (!signer) throw new Error("cancelListing called without a signer");
     if (!deployment) throw new Error("cancelListing was called without a deployment");
-    if (!leverageBuy.listingData) throw new Error("cancelListing called on a non listed LeverageBuy");
     cancelListing({
       signer,
       lbWrapperAddress: deployment.lbWrapperAddress,
@@ -102,6 +122,7 @@ const ListedLB = ({ leverageBuy }: ListedLBProps) => {
     });
   };
 
+  // construct the OpenSea URL of the listing
   const OS_MAINNET_BASE_URL = "https://opensea.io/assets/ethereum";
   const OS_GOERLI_BASE_URL = "https://testnets.opensea.io/assets/goerli";
   const OS_BASE_URL = chain?.id == 1 ? OS_MAINNET_BASE_URL : OS_GOERLI_BASE_URL;
@@ -109,7 +130,9 @@ const ListedLB = ({ leverageBuy }: ListedLBProps) => {
 
   return (
     <>
+      {/* display the listing price*/}
       Listed for {ethers.utils.formatEther(leverageBuy.listingData.listingPrice)} ETH{" "}
+      {/* a link to this listing on OpenSea */}
       <a
         href={osURL}
         target="_blank"
@@ -118,7 +141,8 @@ const ListedLB = ({ leverageBuy }: ListedLBProps) => {
       >
         (View on OpenSea)
       </a>
-      , <Button onClick={delist}>Delist</Button>
+      , {/* Cancel listing button */}
+      <Button onClick={delist}>Delist</Button>
     </>
   );
 };
@@ -127,21 +151,32 @@ interface NotListedLBProps {
   leverageBuy: LeverageBuy;
 }
 
+// NotListedLB is rendered if the NFT is not listed for sale
+// it displays a button that opens the ListForSaleModal
 const NotListedLB = ({ leverageBuy }: NotListedLBProps) => {
+  // current chain
   const { chain } = useNetwork();
+  // list for sale modal state
   const [lfsModalOpen, setLSFModalOpen] = useState(false);
 
+  // callback passed to ListForSaleModal, called after the list for sale transaction succeeds
+  // it posts the order to OpenSea API, to make it visible on their front end
   const postOrderToOpensea = async (order: Order) => {
+    // this is implemented only for Goerli, mainnet needs an API key, and therefore should be handled server side
     if (chain?.id != 5) throw new Error("postOrderToOpensea is implemented on goerli only");
-
+    // request headers
     const headers: HeadersInit = { "content-type": "application/json", accept: "application/json" };
+    // request body, signature is not needed, so we pass "0x"
     const body = JSON.stringify({ parameters: order, signature: "0x" });
+    // send the POST request to this endpoint https://docs.opensea.io/v2.0/reference/create-an-order-testnets
     const response = await fetch("https://testnets-api.opensea.io/v2/orders/goerli/seaport/listings", {
       method: "POST",
       headers,
       body,
     });
     const json = await response.json();
+    // throw if the request fails
+    // this will be caught in the ListForSaleModal, and will show an error on the uI
     if (!response.ok) throw json;
   };
 
@@ -158,6 +193,7 @@ const NotListedLB = ({ leverageBuy }: NotListedLBProps) => {
   );
 };
 
+// helper function that turns an ipfs url to https, used in the below useTokenMetadata hook
 const ipfsToHTTPS = (url: string) => {
   if (url.startsWith("ipfs://")) return `https://ipfs.io/${url.substring(7)}`;
   if (url.startsWith("ipfs://ipfs/")) return `https://ipfs.io/ipfs/${url.substring(7)}`;
@@ -170,6 +206,7 @@ interface TokenMetadata {
   description?: string;
 }
 
+// a hook that fetches an NFT metadata, used in the LBRow component
 const useTokenMetadata = (tokenURI: string) => {
   const url = ipfsToHTTPS(tokenURI);
   const fetcher = async () => {
