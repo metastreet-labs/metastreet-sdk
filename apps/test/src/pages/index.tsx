a39a2843a6c2f66453f5deeac8e8815005cbdd34
Abdou Ouahib <56047563+aouahib@users.noreply.github.com>
2022-11-22 15:52:21 +0100

    chore: refactor BWL feature in test app (#137) (#141)
    * update dependencies
    
    * add Button & Input with minimum styling
    
    * handle hydration error in _app
    
    * refactor BWL feature

--- apps/test/src/pages/index.tsx
+++ apps/test/src/pages/index.tsx
@@ -1,31 +1,12 @@
-import { DEPLOYMENTS, getLeverageBuy, getLeverageBuyEvents } from "@metastreet-labs/margin-core";
-import { useEffect } from "react";
-import { useAccount, useProvider } from "wagmi";
+import { NextPage } from "next";
+import BuyWithLeverageSection from "../components/BuyWithLeverageSection";
 
-const Index = () => {
-  const provider = useProvider();
-  const { address } = useAccount();
-
-  useEffect(() => {
-    const deployment = DEPLOYMENTS[provider.network.chainId];
-    const params = {
-      signerOrProvider: provider,
-      subgraphURI: deployment.subgraphURI,
-      lbWrapperAddress: deployment.lbWrapperAddress,
-    };
-    getLeverageBuy({ escrowID: "0", ...params })
-      .then((json) => console.log({ json }))
-      .catch((e) => console.log(e));
-    getLeverageBuyEvents({ ...params, owner: address, skip: 0, first: 5 })
-      .then((events) => console.log({ events }))
-      .catch((e) => console.log(e));
-  }, [provider, address]);
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.css file.
-   */
-  return <div>Index</div>;
+const Index: NextPage = () => {
+  return (
+    <div className="flex flex-col p-4">
+      <BuyWithLeverageSection />
+    </div>
+  );
 };
 
 export default Index;
