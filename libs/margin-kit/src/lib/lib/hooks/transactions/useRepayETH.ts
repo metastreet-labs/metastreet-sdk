0a2dff498ef3df483ee41e950e06476fba1753c4
Abdou Ouahib <ouahibdou@gmail.com>
2022-11-25 12:59:23 +0100

    add useRepayETH

---
+++ libs/margin-kit/src/lib/lib/hooks/transactions/useRepayETH.ts
@@ -0,0 +1,28 @@
+import { LeverageBuy, repayETH, waitForSubgraphSync } from "@metastreet-labs/margin-core";
+import { useClient, useSigner } from "wagmi";
+import useDeployment from "../../../hooks/useDeployment";
+import { useLeverageBuyEventsQKs } from "../fetchers/subgraph/useLeverageBuyEvents";
+import { useLeverageBuysQKs } from "../fetchers/subgraph/useLeverageBuys";
+
+export const useRepayETH = () => {
+  const { data: signer } = useSigner();
+  const deployment = useDeployment();
+  const { queryClient } = useClient();
+
+  return async (leverageBuy: LeverageBuy) => {
+    if (!signer) throw new Error("repay called without a signer");
+    if (!deployment) throw new Error("repay was called without a deployment");
+    const tx = await repayETH({
+      escrowID: leverageBuy.escrowID,
+      repayment: leverageBuy.repayment,
+      signer,
+      lbWrapperAddress: deployment.lbWrapperAddress,
+    });
+    const receipt = await tx.wait(2);
+    await waitForSubgraphSync({ blockNumber: receipt.blockNumber, subgraphURI: deployment.subgraphURI });
+    const owner = await signer.getAddress();
+    queryClient.invalidateQueries(useLeverageBuysQKs.owner(owner));
+    queryClient.invalidateQueries(useLeverageBuyEventsQKs.owner(owner));
+    return receipt;
+  };
+};
