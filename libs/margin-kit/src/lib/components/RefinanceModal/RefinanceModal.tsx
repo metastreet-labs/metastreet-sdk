c427ab4c70bb8e650c8545bf018c3cae26399f14
Abdou Ouahib <56047563+aouahib@users.noreply.github.com>
2022-12-04 16:44:21 +0100

    feat(Refinance): wait for and invalidate subgraph (#164) (#165)
    chore(Refinance): wait for and invalidate subgraph

--- libs/margin-kit/src/lib/components/RefinanceModal/RefinanceModal.tsx
+++ libs/margin-kit/src/lib/components/RefinanceModal/RefinanceModal.tsx
@@ -1,4 +1,8 @@
 import { LeverageBuy } from "@metastreet-labs/margin-core";
+import { useAccount, useClient } from "wagmi";
+import { useDeployment } from "../../hooks/useDeployment";
+import { useLeverageBuyEventsQKs } from "../../lib/hooks/fetchers/subgraph/useLeverageBuyEvents";
+import { useLeverageBuysQKs } from "../../lib/hooks/fetchers/subgraph/useLeverageBuys";
 import LoanInfoContainer from "../BuyWithLeverageModal/containers/LoanInfoContainer";
 import DefinedDeploymentProvider from "../DefinedDeploymentProvider";
 import MetaStreetModal, { ModalState } from "../MetaStreetModal";
@@ -13,7 +17,18 @@ type RefinanceModalProps = ModalState & {
 };
 
 export const RefinanceModal = (props: RefinanceModalProps) => {
-  const { isOpen, onClose, leverageBuy, ...callbacks } = props;
+  const { isOpen, onClose: ogOnClose, leverageBuy, ...callbacks } = props;
+  const { queryClient } = useClient();
+  const deployment = useDeployment();
+  const { address } = useAccount();
+
+  const onClose = async () => {
+    ogOnClose();
+    if (deployment && address) {
+      queryClient.invalidateQueries(useLeverageBuysQKs.owner(deployment.subgraphURI, address));
+      queryClient.invalidateQueries(useLeverageBuyEventsQKs.owner(deployment.subgraphURI, address));
+    }
+  };
 
   return (
     <MetaStreetModal isOpen={isOpen} onClose={onClose}>
