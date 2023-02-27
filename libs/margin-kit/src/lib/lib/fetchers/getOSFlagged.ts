21a40b520d79d3b5f77cf03be67881b510f53200
András Szücs <andrew0szucs@gmail.com>
2023-02-27 21:21:42 +0100

    fix(kit): separate too many calls in getOSFlagged

--- libs/margin-kit/src/lib/lib/fetchers/getOSFlagged.ts
+++ libs/margin-kit/src/lib/lib/fetchers/getOSFlagged.ts
@@ -12,25 +12,43 @@ interface OpenSeaAsset {
   supports_wyvern: boolean;
 }
 
-const getOSFlagged = async <T extends BaseToken>(tokens: T[]): Promise<GetOSFlaggedResult<T>> => {
-  const urlParams = new URLSearchParams();
-  tokens.forEach((token) => urlParams.append("token_ids", token.tokenID));
-  urlParams.append("asset_contract_address", tokens[0].collectionAddress);
-  const url = `https://api.opensea.io/api/v1/assets?${urlParams}`;
+const LIMIT = 20;
 
-  const response = await fetch(url);
-  const data: { assets: OpenSeaAsset[] } = await response.json();
-  if (!response.ok) throw data;
+const getOSFlagged = async <T extends BaseToken>(tokens: T[]): Promise<GetOSFlaggedResult<T>> => {
+  // chunk the tokens into groups of 30 - OS API has a limit of 30 tokens per request
+  const tokensChunks = [];
+  for (let i = 0; i < tokens.length; i += LIMIT) {
+    const chunk = tokens.slice(i, i + LIMIT);
+    tokensChunks.push(chunk);
+  }
+
+  const flaggedTokenResults = await Promise.all(
+    tokensChunks.map(async (tokensChunk) => {
+      const urlParams = new URLSearchParams();
+      tokensChunk.forEach((token) => urlParams.append("token_ids", token.tokenID));
+      urlParams.append("asset_contract_address", tokensChunk[0].collectionAddress);
+      const url = `https://api.opensea.io/api/v1/assets?${urlParams}`;
+
+      const response = await fetch(url);
+      const data: { assets: OpenSeaAsset[] } = await response.json();
+      if (!response.ok) throw data;
+      return data.assets;
+    })
+  );
+
+  const flaggedTokens = flaggedTokenResults.flat();
 
   const result: GetOSFlaggedResult<T> = {
     flagged: [],
     unflagged: [],
   };
+
   tokens.forEach((token) => {
-    const asset = data.assets.find((a) => a.token_id == token.tokenID);
+    const asset = flaggedTokens.find((a) => a.token_id == token.tokenID);
     if (asset?.supports_wyvern) result.unflagged.push(token);
     else result.flagged.push(token);
   });
+
   return result;
 };
 
