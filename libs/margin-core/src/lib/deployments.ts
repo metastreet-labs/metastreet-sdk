29319cef48533ed447f8404f5a5cbdec46a93b96
András Szücs <andrew0szucs@gmail.com>
2023-02-27 21:36:50 +0100

    chore(core): add multicallContractAddress to deployments

--- libs/margin-core/src/lib/deployments.ts
+++ libs/margin-core/src/lib/deployments.ts
@@ -7,6 +7,7 @@ export const CHAIN_IDS = {
 export interface Deployment {
   vaults: string[];
   lbWrapperAddress: string;
+  multicallContractAddress: string;
   subgraphURI: string;
   reservoirURL: string;
 }
@@ -15,6 +16,7 @@ export const defaultDeployments: Record<number, Deployment> = {
   1: {
     vaults: ["0x7770cd73e035c37bdf8875eee81577c63202ab8d"],
     lbWrapperAddress: "0x558aD8278B6D127dc8F58e02Ee578Df20ec98406",
+    multicallContractAddress: "0xcA11bde05977b3631167028862bE2a173976CA11",
     subgraphURI:
       "https://gateway.thegraph.com/api/3da96fefdc83e3aee6b1fbe05079077f/subgraphs/id/HPvcWgsjmQG1U7YeS9RRMqYpQZc2RH8wrd3zatpb3AS6",
     reservoirURL: "https://api.reservoir.tools",
@@ -22,6 +24,7 @@ export const defaultDeployments: Record<number, Deployment> = {
   5: {
     vaults: ["0x99156878c19c46c492d3ac40a3a8483c3740a9d5", "0x823905D12Ef070eDBE19aD35Bcd1fe719Dd94016"],
     lbWrapperAddress: "0x5D17Fb028eB1D4aC437e2E94aB0A47146d6cC2Ce",
+    multicallContractAddress: "0xcA11bde05977b3631167028862bE2a173976CA11",
     subgraphURI: "https://api.studio.thegraph.com/query/31830/metastreet-lb-subgraph-goerli/v1.0.0",
     reservoirURL: "https://api-goerli.reservoir.tools",
   },
